6d6323e0afef633e61fa931e200211914c0d31c6
Clark Bulleit <clark.bulleit@duke.edu>
2018-09-11 15:52:37 -0400

    First commit for average of three numbers

---
+++ firstpython.py
@@ -0,0 +1,25 @@
+def main():
+	add_three(1,2,3)
+	average_three(p)
+	
+def add_three(v1,v2,v3):
+	""" add three numbers
+	
+	:param v1: number one
+	:param v2: number two
+	:param v3: number three
+	"""
+	p = v1+v2+v3
+	
+	return p
+
+def average_three():
+	""" divide sum by three
+	"""
+
+	q = p/3
+	
+	print(q)
+if _name_ == "_main_":
+	main()
+
